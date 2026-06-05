79a1d82be041fc55ba6e8d839d392a5ed84d20ce
Danielle Dockery <danixielle@gmail.com>
2026-06-05 12:30:12 -0400

    Initial Commit

---
+++ VoicePDFMemo/VoicePDFMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicePDFMemoApp.swift
+//  VoicePDFMemo
+//
+//  Created by Danielle Dockery on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct VoicePDFMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
